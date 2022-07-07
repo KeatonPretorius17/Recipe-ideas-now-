878fac5d891a4988c8522e772edfc0c003ee6fa1
keaton <keat0117@gmail.com>
2022-07-07 21:41:23 +0100

    Changed styles

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'd2850d68-7dd8-4406-be9d-4217fe7c11e8';
 export const MODAL_CLOSE_SEC = 2.5;
